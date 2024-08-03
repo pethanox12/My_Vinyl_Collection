7b3f4daf372297325065d7b52546b358600adccf
Adrian Talbot <adrian.r.talbot@gmail.com>
2024-08-03 11:47:21 +0100

    Add .gitignore for XCode and Swift Add config file and exclude from tracking Add UID and PASSWD to configuration Add fields to display UID and PASSWD

--- My Vinyl Collection/ContentView.swift
+++ My Vinyl Collection/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var uid: String = ""
+    @State private var passwd: String = ""
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Login credentials")
+                .font(.largeTitle)
+                .bold()
+            
+            HStack {
+                Text("Username:")
+                    .bold()
+                TextField("User id", text: $uid)
+                    .textFieldStyle(.roundedBorder)
+                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                Spacer()
+            }
+            
+            HStack {
+                Text("Password:")
+                    .bold()
+                
+                SecureField("Password", text: $passwd)
+                    .textFieldStyle(.roundedBorder)
+                    .frame(width: 300)
+                
+                Button("Go") {
+                    // ToDo: Process the user's input
+                }
+                .padding()
+                
+                Spacer()
+            }
         }
         .padding()
+        .onAppear(perform: {
+            
+            uid = Bundle.main.infoDictionary?["UID"] as! String
+            
+            passwd = Bundle.main.infoDictionary?["PASSWD"] as! String
+        })
     }
 }
 
