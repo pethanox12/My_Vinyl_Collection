8701ef9efe85af1013df6258879de7b553e22a36
Adrian Talbot <adrian.r.talbot@gmail.com>
2024-08-03 11:00:54 +0100

    Initial Commit

---
+++ My Vinyl Collection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  My Vinyl Collection
+//
+//  Created by Adrian Talbot on 03/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ My Vinyl Collection/My_Vinyl_CollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Vinyl_CollectionApp.swift
+//  My Vinyl Collection
+//
+//  Created by Adrian Talbot on 03/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct My_Vinyl_CollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
